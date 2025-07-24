798d6c11d608cdbe3c7f016a5140e27024a83bdc
Priyansh Malik <Manoj_Pandey@steris.com>
2025-07-24 13:56:54 +0530

    #66342 :- Fixed the issue of not shwoing alias in edit mode

--- src/lib/components/Form/fields/chipInput.js
+++ src/lib/components/Form/fields/chipInput.js
@@ -41,8 +41,10 @@ const Field = ({ isAdd, column, field, formik, otherProps, fieldConfigs = {}, mo
                         {...params} 
                         variant="standard"
                         InputProps={{
+                            ...params.InputProps,
                             sx: {
-                                backgroundColor: isDisabled ? '#dfdede' : 'transparent'
+                                ...params.InputProps?.sx,
+                                ...(isDisabled && { backgroundColor: '#dfdede' })
                             }
                         }}
                     />
