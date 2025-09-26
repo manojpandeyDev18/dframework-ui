8d8d9ff6b58d30a1813a9fe250739875bbc29f94
Priyansh Malik <Manoj_Pandey@steris.com>
2025-09-26 17:39:38 +0530

    #650442 :- Removed customInput Component

--- dist/components/Grid/index.js
+++ dist/components/Grid/index.js
@@ -79,7 +79,6 @@ var _Box = _interopRequireDefault(require("@mui/material/Box"));
 const _excluded = ["model", "columns", "api", "defaultSort", "setActiveRecord", "parentFilters", "parent", "where", "title", "permissions", "selected", "assigned", "available", "disableCellRedirect", "onAssignChange", "customStyle", "onCellClick", "showRowsSelected", "chartFilters", "clearChartFilter", "showFullScreenLoader", "customFilters", "onRowDoubleClick", "onRowClick", "gridStyle", "reRenderKey", "additionalFilters", "onCellDoubleClickOverride", "onAddOverride", "dynamicColumns", "readOnly", "baseFilters"];
 function _interopRequireWildcard(e, t) { if ("function" == typeof WeakMap) var r = new WeakMap(), n = new WeakMap(); return (_interopRequireWildcard = function _interopRequireWildcard(e, t) { if (!t && e && e.__esModule) return e; var o, i, f = { __proto__: null, default: e }; if (null === e || "object" != typeof e && "function" != typeof e) return f; if (o = t ? n : r) { if (o.has(e)) return o.get(e); o.set(e, f); } for (const t in e) "default" !== t && {}.hasOwnProperty.call(e, t) && ((i = (o = Object.defineProperty) && Object.getOwnPropertyDescriptor(e, t)) && (i.get || i.set) ? o(f, t, i) : f[t] = e[t]); return f; })(e, t); }
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
-function _extends() { return _extends = Object.assign ? Object.assign.bind() : function (n) { for (var e = 1; e < arguments.length; e++) { var t = arguments[e]; for (var r in t) ({}).hasOwnProperty.call(t, r) && (n[r] = t[r]); } return n; }, _extends.apply(null, arguments); }
 function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
 function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
 function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
@@ -551,23 +550,7 @@ const GridBase = /*#__PURE__*/(0, _react.memo)(_ref => {
         if (column.type === constants.boolean) {
           operators = (0, _xDataGridPremium.getGridBooleanOperators)();
         }
-        overrides.filterOperators = operators.map(operator => _objectSpread(_objectSpread({}, operator), {}, {
-          InputComponent: operator.InputComponent ? params => /*#__PURE__*/_react.default.createElement(_CustomDropdownMenu.default, _extends({
-            column: _objectSpread(_objectSpread(_objectSpread({}, column), column.type === constants.boolean ? {
-              customLookup: [{
-                value: true,
-                label: 'Yes'
-              }, {
-                value: false,
-                label: 'No'
-              }]
-            } : {}), {}, {
-              dataRef
-            })
-          }, params, {
-            autoHighlight: true
-          })) : undefined
-        }));
+        overrides.filterOperators = operators.map(operator => _objectSpread({}, operator));
       }
       if (column.linkTo || column.link) {
         overrides.cellClassName = 'mui-grid-linkColumn';
