246a586db46e610361b1551201a228367f402822
Priyansh Malik <Manoj_Pandey@steris.com>
2025-09-29 05:00:38 +0530

    Destructrured label from otherprops

--- src/lib/components/Form/fields/string.js
+++ src/lib/components/Form/fields/string.js
@@ -1,8 +1,9 @@
 import TextField from '@mui/material/TextField';
 import React from 'react';
 
-const field = ({ column, field, formik, ...otherProps }) => {
+const field = ({ column, field, formik, label, ...otherProps }) => {
     const rows = column.rows || (column.multiline ? 5 : 1);
+    console.log("Other props are ", otherProps);
     return <TextField
         type="text"
         variant={column.variant || "standard"}
