d8ec4af83cff928cb5e1785920a291e6cf1f7caf
Tushar Banswal <tushar_banswal@steris.com>
2025-10-24 19:54:42 +0530

    Fix logic for recordEditable and readOnlyRelations in Form component for clarity

--- src/lib/components/Form/Form.js
+++ src/lib/components/Form/Form.js
@@ -288,8 +288,8 @@ const Form = ({
   ];
   const showRelations = Number(id) !== 0 && Boolean(relations.length);
   const showSaveButton = searchParams.has("showRelation");
-  const recordEditable = (!("canEdit" in data) || data.canEdit);
-  const readOnlyRelations = (!recordEditable || data.readOnlyRelations);
+  const recordEditable = !("canEdit" in data) || data.canEdit;
+  const readOnlyRelations = !recordEditable || data.readOnlyRelations;
   deletePromptText = deletePromptText || "Are you sure you want to delete ?";
   return (
     <>
